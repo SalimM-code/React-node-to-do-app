9b25861663522767d8f2143b4a83837a1b2caf61
salimM-code <mustafasalim000@gmail.com>
2022-01-07 13:50:27 -0500

    created a new component

--- client/src/components/CreateTodo.jsx
+++ client/src/components/CreateTodo.jsx
@@ -1,22 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "./styles/createNew.styled";
 import  Checkbox  from "@mui/material/Checkbox";
+import axios from "axios";
 
-const CreateTodo = () => {
+const CreateTodo = (props) => {
+  const { data, setData } = props;
   const [newTodo, setNewTodo] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    axios.post('/api/todo/new', {
+      name: newTodo
+    }).then(res => {
+
+      setData(res.data.data);
+      setNewTodo("");
+    })
   }
   return ( 
     <Container className="input-field">
-      {/* <button className="checkmarkButton">
-        <div className="checkmarkBorder">
-          <div className="checkmark"></div>
-        </div>
-      </button> */}
       <Checkbox disabled/>
-
       <form onSubmit={handleSubmit} className="newTodo">
         <input className="input"
           value={newTodo}
